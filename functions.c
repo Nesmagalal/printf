732966a5022b7c1a9ee05843a90b1975394764de
”fatema-zahraa” <drzahraaahmad@gmail.com>
2023-05-28 01:23:32 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include "main.h"
+/************************* PRINT CHAR *************************/
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -15,6 +16,7 @@ int flags, int width, int precision, int size)
 char c = va_arg(types, int);
 return (handle_write_char(c, buffer, flags, width, precision, size));
 }
+/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -30,6 +32,7 @@ int flags, int width, int precision, int size)
 {
 int length = 0, i;
 char *str = va_arg(types, char *);
+
 UNUSED(buffer);
 UNUSED(flags);
 UNUSED(width);
@@ -41,10 +44,8 @@ str = "(null)";
 if (precision >= 6)
 str = "      ";
 }
-
 while (str[length] != '\0')
 length++;
-
 if (precision >= 0 && precision < length)
 length = precision;
 
@@ -67,6 +68,7 @@ return (width);
 }
 return (write(1, str, length));
 }
+/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -78,16 +80,18 @@ return (write(1, str, length));
  * Return: Number of chars printed
  */
 int print_percent(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-UNUSED(types);
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-return (write(1, "%%", 1));
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
+
+/************************* PRINT INT *************************/
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -99,34 +103,39 @@ return (write(1, "%%", 1));
  * Return: Number of chars printed
  */
 int print_int(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-int i = BUFF_SIZE - 2;
-int is_negative = 0;
-long int n = va_arg(types, long int);
-unsigned long int num;
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
 
-n = convert_size_number(n, size);
-if (n == 0)
-buffer[i--] = '0';
+	n = convert_size_number(n, size);
 
-buffer[BUFF_SIZE - 1] = '\0';
-num = (unsigned long int)n;
+	if (n == 0)
+		buffer[i--] = '0';
 
-if (n < 0)
-{
-num = (unsigned long int)((-1) * n);
-is_negative = 1;
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
-while (num > 0)
-{
-buffer[i--] = (num % 10) + '0';
-num /= 10;
-}
-i++;
-return (write_number(is_negative, i, buffer, flags, width, precision, size));
-}
+/************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
@@ -138,33 +147,37 @@ return (write_number(is_negative, i, buffer, flags, width, precision, size));
  * Return: Numbers of char printed.
  */
 int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-unsigned int n, m, i, sum;
-unsigned int a[32];
-int count;
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
-n = va_arg(types, unsigned int);
-m = 2147483648; /* (2 ^ 31) */
-a[0] = n / m;
-for (i = 1; i < 32; i++)
-{
-m /= 2;
-a[i] = (n / m) % 2;
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
-for (i = 0, sum = 0, count = 0; i < 32; i++)
-{
-sum += a[i];
-if (sum || i == 31)
-{
-char z = '0' + a[i];
-write(1, &z, 1);
-count++;
-}
-}
-return (count);
-}
+
